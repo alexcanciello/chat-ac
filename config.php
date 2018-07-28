a55d561a266c3c0efc07583e7d057f3e284aa435
alexcanciello <41766583+alexcanciello@users.noreply.github.com>
2018-07-28 04:19:10 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
 $access_token = "EAAHKVYWmtnABAAEzZAkyowwK34Kobzsk2fDAendZB68EMWR2BatFFM3fa4aMZCzrRhGzn3pS6abO5S4MtKTDK0YZA8Rk9gvTRERmvARUEvPGMCEyjKtNXwM5LIUXY9P8wPiRVWdvZBvw6vm8e0RTnZCinZCACFkjIeO4JQGJZAvzhQbiWAjbMzPT";
 
 // Your webhook varification token
-$verify_token = "1029384756";
+$verify_token = "fb_time_bot";
 
 ?>
