dd9943214046a4d0356148b1d7b90e97dd41e980
alexcanciello <41766583+alexcanciello@users.noreply.github.com>
2018-07-28 05:13:30 +0200

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Your Page Access Token
-$access_token = "EAAHKVYWmtnABAL8jfTjWMqxmZCvw4V6rfDUaMUrxzxdEUjPte5ljhyy7doRNhsMB2ylO2JWpAdrO8mO3VKvLjIFfc6n6tZAqt2c6JtERj2oJd4PGJvXZCGh5y0OHtRONUH05HqgCthPUKWBWqwGEwwLxki23MNTaFaKFszBChc9sblArmHk";
-
-// Your webhook varification token
-$verify_token = "fb_time_bot";
-
-?>
