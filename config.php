d1af0482911361f4a79d2a943cbf50ac49b03c0d
alexcanciello <41766583+alexcanciello@users.noreply.github.com>
2018-07-28 04:46:06 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Your Page Access Token
-$access_token = "EAAHKVYWmtnABAMnm3d6MkqKhQchShDcUEgD0wbZBEfvuuDxivrPAVUuqJHocGAHCYTgi4jkj46eTta76zIsI8ZAkClLV1NKXrr8X6UCmeCOgR5Ex5aO7piBi3mU4FBenmj4ZAkOZCXhSvmJKZCZCnZBvNHLSAZCdrEdIJUhdAelkxagGZArv6r7yz";
+$access_token = "EAAEJR7nPEecBAMeSP79RmMb5Hf6av5cHc2NryfyWrYQemvJgVXEQojCY2ZBoGguCtd3lgSJzDvW0YonEdETwNkTO7XYfTYlmkmTcRgSneUUUeNDv4f6136oUTnpxkAhTef41rFPpwZCu0FW9yZASQIRNowc7xazmkeWqIyBc4PUvF0dB1RA";
 
 // Your webhook varification token
 $verify_token = "fb_time_bot";
