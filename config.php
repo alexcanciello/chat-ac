74606f8d05cc5f9084628b2fafc0751d249231ab
alexcanciello <41766583+alexcanciello@users.noreply.github.com>
2018-07-28 05:14:01 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'verify_token' => 'EAAHKVYWmtnABAL8jfTjWMqxmZCvw4V6rfDUaMUrxzxdEUjPte5ljhyy7doRNhsMB2ylO2JWpAdrO8mO3VKvLjIFfc6n6tZAqt2c6JtERj2oJd4PGJvXZCGh5y0OHtRONUH05HqgCthPUKWBWqwGEwwLxki23MNTaFaKFszBChc9sblArmHk',
+    'token' => '1234'
+];
